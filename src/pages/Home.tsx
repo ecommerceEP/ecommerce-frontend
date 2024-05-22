0b8bd4ff11a901b8855d9695fa971a25857ae7bc
giovannacilfone <giovannacilfone2@gmail.com>
2024-05-22 18:18:16 -0300

    template-ecommerce

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant="h4">
+        <HomeIcon /> Home Page
+      </Typography>
+    </div>
+  );
+};
+
+export default Home;
